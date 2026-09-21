89608f599cdcf05a19c2678f4f29e50dbae862d0
Danil Ukhov <danil.u@bnovo.ru>
2026-09-21 11:48:10 +0700

    chore: MSW настройка. Время: 20 мин

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+if (import.meta.env.VITE_USE_MOCK !== 'false') {
+  const { worker } = await import('./mock/browser')
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
 const app = createApp(App)
 
 app.use(router)
